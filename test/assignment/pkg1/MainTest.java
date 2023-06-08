6c567a50e1999c5a40f1cb7930e2444f1a1d43bb
Shaneel <Shaneel@192.168.1.121>
2023-06-09 11:44:53 +1200

    Main Class Test

--- test/assignment/pkg1/MainTest.java
+++ test/assignment/pkg1/MainTest.java
@@ -18,5 +18,72 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
-    
+    @Test
+    public void testPlayerHit() throws Exception {
+        Player player = new Player("you");
+        Deck deck = new Deck();
+        Card card1 = deck.nextCard();
+        player.setCard1(card1);
+        Card card2 = deck.nextCard();
+        player.setCard2(card2);
+        
+        assertNotNull(player.getCard1());
+        assertNotNull(player.getCard2());
+    }
+
+    @Test
+    public void testPlayerStand() throws Exception {
+        Player player = new Player("you");
+        Deck deck = new Deck();
+        Card card1 = deck.nextCard();
+        player.setCard1(card1);
+        Card card2 = deck.nextCard();
+        player.setCard2(card2);
+
+        assertTrue(player.getCard1().getValue() + player.getCard2().getValue() <= 21);
+    }
+
+    @Test
+    public void testPlayerBust() throws Exception {
+        Player player = new Player("you");
+        Deck deck = new Deck();
+        Card card1 = deck.nextCard();
+        player.setCard1(card1);
+        Card card2 = deck.nextCard();
+        player.setCard2(card2);
+
+        int total = card1.getValue() + card2.getValue();
+        if (total > 21) {
+            card1.setValue(1);
+            card2.setValue(1);
+            total = 2;
+        }
+
+        player.setTotal(22);
+
+        assertTrue(player.getTotal() > 21);
+    }
+
+    @Test
+    public void testPlayerBlackjack() throws Exception {
+        Player player = new Player("you");
+        Dealer dealer = new Dealer("dealer");
+
+        player.setTotal(21);
+        dealer.setTotal(21);
+
+        assertEquals(21, player.getTotal());
+        assertEquals(21, dealer.getTotal());
+    }
+
+    @Test
+    public void testPlayerWin() throws Exception {
+        Player player = new Player("you");
+        Dealer dealer = new Dealer("dealer");
+
+        player.setTotal(20);
+        dealer.setTotal(15);
+
+        assertTrue(player.getTotal() < 21 && player.getTotal() > dealer.getTotal());
+    }
 }
